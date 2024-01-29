b1e9e29e4918a3776ede3c8ff6740f9cfbd77a0a
Anni-Marleena <anni-marleena.eno@hotmail.com>
2024-01-29 19:45:54 +0200

    week 3 assignment

---
+++ web-hybriditek-mobiiliohj/login-form/App.js
@@ -0,0 +1,10 @@
+import { PaperProvider } from 'react-native-paper';
+import Login from './screens/Login';
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <Login />
+    </PaperProvider>
+  );
+}
